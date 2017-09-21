5b35316f57d9915ba6975506a5524f1ddc1496c8
Alexander Zhidkov <alexander.zhidkov@empired.com>
2017-09-21 21:29:13 +0800

    show snack bar on send

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
+import { MdSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-contact',
@@ -9,10 +10,15 @@ import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/data
 export class ContactComponent implements OnInit {
   contact: FirebaseObjectObservable<any>;
 
-  constructor(private db: AngularFireDatabase) { }
+  constructor(private db: AngularFireDatabase, public snackBar: MdSnackBar) { }
 
   ngOnInit() {
     this.contact = this.db.object(`/users/${localStorage.getItem('currentUserId')}/contact`);
   }
 
+  openSnackBar() {
+    this.snackBar.open('Your message submitted', 'ok', {
+      duration: 3000
+    });
+  }
 }
