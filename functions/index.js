57e1c042cbde1ed1804eb8a56f16208c6447ce2d
Alexander Zhidkov <alexander.zhidkov@empired.com>
2017-10-24 21:55:06 +0800

    add contactMessageToSlack function

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,22 @@ const functions = require('firebase-functions');
 
 // Sends user feedback to Slack channel.
 const rp = require('request-promise');
+
+exports.contactMessageToSlack = functions.database.ref('/contact/{messageId}').onWrite(event => {
+    const snapshot = event.data;
+    const contact = snapshot.val();
+
+    const message = `*${contact.name}*, ${contact.email} ${contact.phone}. *Message:* ${contact.message}`;
+    return rp({
+        method: 'POST',
+        uri: functions.config().slack.webhook_url,
+        body: {
+            text: message
+        },
+        json: true
+    });
+});
+
 exports.feedbackToSlack = functions.database.ref('/feedback/{feedbackId}').onWrite(event => {
     const snapshot = event.data;
     const feedback = snapshot.val();
